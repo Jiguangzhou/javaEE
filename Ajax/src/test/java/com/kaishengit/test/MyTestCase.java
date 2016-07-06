3f840ab5e3616916f126e307f26fbc16c9755040
Jiguangzhou <jgz1009@126.com>
2016-07-06 14:01:06 +0800

    -修改了Test

--- Ajax/src/test/java/com/kaishengit/test/MyTestCase.java
+++ Ajax/src/test/java/com/kaishengit/test/MyTestCase.java
@@ -5,13 +5,13 @@ import org.junit.Test;
 public class MyTestCase {
 
     @Test
-    public void testTel(){
-        int[] arr = new int[]{5,9,1,0,3,6};
-        int[] index = new int[]{2,4,2,5,5,3,5,0,4,1,5};
-        String tel ="";
-        for (int i:index){
-            tel+= arr[i];
+    public void testTel() {
+        int[] arr = new int[]{5, 9, 1, 0, 3, 6};
+        int[] index = new int[]{2, 4, 2, 5, 5, 3, 5, 0, 4, 1, 5};
+        String tel = "";
+        for (int i : index) {
+            tel += arr[i];
         }
-        System.out.println("联系方式:"+tel);
+        System.out.println("联系方式:" + tel);
     }
 }
